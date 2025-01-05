45716d2d79f3e866eef7b1f8190ce888b8bc8b49
KyujungLee <leekyujung123@gmail.com>
2025-01-05 18:31:29 +0900

    주석 ã추가

--- calculator1/calculator1/src/main/java/com/example/calculator2/Except.java
+++ calculator1/calculator1/src/main/java/com/example/calculator2/Except.java
@@ -29,23 +29,24 @@ public class Except {
                 break;
             }
         }
-        if (count > 0) {
+
+        if (count > 0) { // 첫번째, 세번째 입력값이 숫자인지 검증, 아니라면 처음으로 돌아감.
             System.out.println("올바른 숫자를 입력하세요.");
             state = "restart";
             return state;
         } else if (!operator.equals("+") && !operator.equals("-")&&
-                !operator.equals("*") && !operator.equals("/")) {
+                !operator.equals("*") && !operator.equals("/")) { // 올바르지 않은 연산자 검증
             System.out.println("올바른 연산자를 입력하세요");
             state = "restart";
             return state;
         } else {
             num1 = Integer.parseInt(start);
             num2 = Integer.parseInt(end);
-            if (num1 < 0 || num2 < 0) {
+            if (num1 < 0 || num2 < 0) { // 첫번째, 세번째 입력값이 음의 값을 가질 경우, 처음으로 돌아감.
                 System.out.println("양의 정수를 입력하세요.");
                 state = "restart";
                 return state;
-            } else if(num2==0){
+            } else if(num2==0){ // 세번째 입력값이 0일 경우, 처음으로 돌아감.
                 System.out.println("0으로 나눌 수 없습니다. 다시 입력하세요.");
                 state = "restart";
                 return state;
--- calculator1/calculator1/src/main/java/com/example/calculator2/Main.java
+++ calculator1/calculator1/src/main/java/com/example/calculator2/Main.java
@@ -8,6 +8,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
+            // 첫번째 값을 입력받음
+            // 첫번째 값이 "exit"라면 프로그램을 종료
+            // 두번째, 세번째 값을 입력받음.
+            // 3가지 입력값을 Except.handling() 매서드를 통해 예외사항 검증
+            // execpt.handling 매서드의 리턴값 state가 "restart"라면, 프로그램 재시작
 
             System.out.println("양의 정수(0을 포함)와 연산자를, 엔터로 구분하여 입력하세요. exit를 입력하면 종료됩니다.");
             String state="";
